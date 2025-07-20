4b61279cdc9841fcac2227ddc40e8b9698e97be4
garry7779 <yashmalvade62@gmail.com>
2025-07-20 17:08:21 +0530

    First commit

---
+++ springdemoapplication/src/main/java/Tech/Code/springdemoapplication/SpringdemoapplicationApplication.java
@@ -0,0 +1,13 @@
+package Tech.Code.springdemoapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdemoapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdemoapplicationApplication.class, args);
+	}
+
+}
---
+++ springdemoapplication/src/test/java/Tech/Code/springdemoapplication/SpringdemoapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package Tech.Code.springdemoapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
